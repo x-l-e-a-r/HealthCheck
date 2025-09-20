67690b76b03e348629c854129e8367a711622a60
xlear <harringtonmatthewc@gmail.com>
2025-09-20 15:33:52 -0500

    Add | BaseHttpServiceExtensions test class

---
+++ tests/HealthCheck.Extensions.Common.Tests/BaseHttpServiceExtensionsAddNamedClientShould.cs
@@ -0,0 +1,15 @@
+﻿namespace HealthCheck.Extensions.Common.Tests;
+
+public class BaseHttpServiceExtensionsAddNamedClientShould
+{
+    public BaseHttpServiceExtensionsAddNamedClientShould()
+    {
+        
+    }
+
+    [Fact]
+    public async Task AddNamedClientToDependencyPipeline_When_AddNamedClientCalled()
+    {
+        
+    }
+}
